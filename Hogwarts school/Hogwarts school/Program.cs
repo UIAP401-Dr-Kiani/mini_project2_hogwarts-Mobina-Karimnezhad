58db1263d484eaf8b6844653f4fa37c64ef8ca19
Mobina <m.karimnejad314@gmail.com>
2023-04-26 00:31:55 -0700

    File added

---
+++ Hogwarts school/Hogwarts school/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts_school
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
