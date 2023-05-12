473d2b1b40071497687bc441f7309598bbaadf3f
Mobina <m.karimnejad314@gmail.com>
2023-05-12 06:21:25 -0700

    Homework function and T_menu function were created for teacher.

--- Hogwarts school/Hogwarts school/Teacher.cs
+++ Hogwarts school/Hogwarts school/Teacher.cs
@@ -9,6 +9,27 @@ namespace Hogwarts_school
     class Teacher : Allowed_People
     {
         public bool Same_Time { get; set; }
-        
+        //-------------------------------- Home Work ---------------------------
+        public void HomeWork()
+        {
+            string Homework = Console.ReadLine();
+            Console.WriteLine($"Exercise :\n{Homework}\n Good job!");
+        }
+        //-------------------------------------- Teacher menu ------------------------------------
+        public void T_menu()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" 1.Add Exercise \n 2. \n 3. ");
+            Console.ResetColor();
+            int Tchr_Ch = Console.Read();
+
+            if (Tchr_Ch == 1)
+                HomeWork();
+
+
+
+        }
+
+
     }
 }
